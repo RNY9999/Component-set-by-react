b25cb73907b2049e9e372e52d3c5f84b572d971e
renya_hisamatsu <renya.hisamatsu@g.dreamcareer.co.jp>
2024-04-25 19:08:12 +0900

    対応issue：#1 作業内容 ・react-hook-form のインストール ・ログインフォームの作成
    備考
    ログインフォームの作成についてはcssは未適用なので次回はcss適用と多少の修正を行ってください。

--- README.md
+++ README.md
@@ -6,10 +6,16 @@
 
 カレントディレクトリで展開するコマンド
 
-> npx create-react-spp .
+`npx create-react-spp .`
 
 ### memo
 
 gitignoreに/node_modulesを加えるのを忘れると大変なことになります。
 
 そのうちreadmeをきれいにまとめます...
+
+## react-fook-form install手順
+
+参考サイト：https://react-hook-form.com/get-started
+
+`npm install react-hook-form`
--- src/components/object/project/LoginForm.jsx
+++ src/components/object/project/LoginForm.jsx
@@ -1,30 +1,51 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
+import { useForm, SubmitHandler } from "react-hook-form";
 
-import Button from '../component/Button';
-import Form from '../component/Form';
+// import Button from '../component/Button';
+// import Form from '../component/Form';
 
-import { handleInputChange } from '../../localLibrary/formLib';
+// import { handleInputChange } from '../../localLibrary/formLib';
 
 const LoginForm = () => {
-    const [userName, setUserName] = useState('初期値');
-  return (
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors, isValid },
+    } = useForm();
+
+    const onSubmit = (data) => console.log(data);
+    console.log(`errors:${isValid}`);
+    return (
     <>
-        <div>{userName}</div>
-        <form action="" className='login-form'>
-            <Form
-                type="text"
-                className="login-form__user-name"
-                setUserName={setUserName}
-                onChange={handleInputChange}
-            />
-            <Button
-                type="submit"
-                className="login-form__submit"
-                text="ログイン"
-            />
+        <form className='login-form' action="" onSubmit={handleSubmit(onSubmit)}>
+            <label htmlFor="userName">ユーザーネーム</label>
+            <input className='login-form__user-name' id='userName' type="text" {
+                ...register('userName', {
+                    required: 'required user name',
+                    minLength: {
+                        value: 4,
+                        message: '4文字以上で入力してください。'
+                    },
+                    maxLength: {
+                        value: 16,
+                        message: '16文字以下で入力してください。'
+                    }
+                })
+            }/> 
+            <p>
+                {errors?.userName?.message}
+            </p>
+            <label htmlFor="userPassword">パスワード</label>
+            <input className='login-form__user-pass' id='userPassword'   type="password" {...register('userPassword', {required: 'required password'})}/>
+            <p>
+                {errors?.userPassword?.message}
+            </p>
+            {console.log(errors.userPassword)}
+            <button className='login-form__submit' type='submit' disabled={!isValid}>ログイン</button>
         </form>
     </>
-  )
+    )
 }
 
 export default LoginForm
